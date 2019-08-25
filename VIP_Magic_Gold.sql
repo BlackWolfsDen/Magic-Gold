d20f73116e104d2ef0456056571fc2c4d209f7e2
Grumbo <blackwolfsden@hotmail.com>
2019-08-24 22:14:51 -0600

    Add files via upload

---
+++ VIP_Magic_Gold.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `item_template` (`entry`, `class`, `subclass`, `SoundOverrideSubclass`, `name`, `displayid`, `Quality`, `Flags`, `FlagsExtra`, `BuyCount`, `AllowableClass`, `AllowableRace`, `stackable`, `bonding`, `description`, `BagFamily`) VALUES 
+(44209, 10, 0, -1, 'Magic Gold', 32282, 1, 64, 0, 1, -1, -1, 2147483647, 0, 'custom currency.', 8192);
+
