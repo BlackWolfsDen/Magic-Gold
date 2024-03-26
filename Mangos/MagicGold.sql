e32c151477e3cc7a1c6c9477f415b4846b7357d1
Grumbo <blackwolfsden@hotmail.com>
2024-03-26 13:26:49 -0600

    Create MagicGold.sql

---
+++ Mangos/MagicGold.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `item_template` (`entry`, `class`, `subclass`, `unk0`, `name`, `displayid`, `Quality`, `Flags`, `Flags2`, `BuyCount`, `AllowableClass`, `AllowableRace`, `stackable`, `bonding`, `description`, `BagFamily`) VALUES 
+(44209, 10, 0, -1, 'Magic Gold', 32282, 1, 64, 0, 1, -1, -1, 2147483647, 0, 'custom currency.', 8192);
